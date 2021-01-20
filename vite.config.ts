a53a8dcdb6e8e0337e2da85ad5d4263ddc232efa
NineSwordsMonster <wangjia_1919@163.com>
2021-01-20 14:11:01 +0800

    support jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
-
 export default defineConfig({
-  plugins: [vue()]
+    // alias: {
+    //     '/@/': path.resolve(__dirname, './src'),
+    //     '/@views/': path.resolve(__dirname, './src/views'),
+    //     '/@components/': path.resolve(__dirname, './src/components'),
+    //     '/@utils/': path.resolve(__dirname, './src/utils'),
+    // },
+    plugins: [
+        vue(),
+        vueJsx({
+            // options are passed on to @vue/babel-plugin-jsx
+        })
+    ]
 })
